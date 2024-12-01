1229e0033aff20fd143b69797579f1ea68259a4c
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-01 07:44:20 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//*****************************************************************************
+//** 1346. Check if N and Its Double Exist    leetcode                       **
+//*****************************************************************************
+
+bool checkIfExist(int* arr, int arrSize) 
+{
+    for(int i = 0; i < arrSize; i++) 
+    {
+        for(int j = 0; j < arrSize; j++) 
+        {
+            if(i != j && arr[i] == 2 * arr[j]) 
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
